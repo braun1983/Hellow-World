29e54b251abcee8469c200bb6677dd9f7d528183
Raphael Brun <73260233+braun1983@users.noreply.github.com>
2022-12-09 12:15:18 +0000

    Initial commit

---
+++ aula04-sintaxe e tipos de dados.js
@@ -0,0 +1,17 @@
+idade = 20;
+altura = 1.75;
+nome = "José";
+pagou = true;
+
+console.log ("Os valores atribuídos as variáveis são:");
+console.log ("Idade:", idade);
+console.log ("Altura:", altura);
+console.log ("Nome:", nome);
+console.log ("Pagou:", pagou);
+
+
+console.log ("\nOs tipos de variáveis são:");
+console.log ("Idade:", typeof (idade));
+console.log ("Altura:", typeof (altura));
+console.log ("Nome:", typeof (nome));
+console.log ("Pagou:", typeof (pagou));
