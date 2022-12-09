e721a2a27527f346b40139a54bfbb873d7e80dcc
Raphael Brun <73260233+braun1983@users.noreply.github.com>
2022-12-09 13:08:40 +0000

    06-entrada de dados

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const prompt = require('prompt-sync')();
+
+var nome = prompt("Digite seu nome:");
+var salario_bruto = parseFloat(prompt(" Digite seu salário bruto:"));
+var ir = salario_bruto * 0.10;
+var salario_liquido = salario_bruto - ir;
+console.log("Salário líquido: R$", salario_liquido);
