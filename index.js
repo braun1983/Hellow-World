4509fcb86f992bf4772520fe874afd28d3e2c5d5
Raphael Brun <73260233+braun1983@users.noreply.github.com>
2023-01-10 18:23:37 +0000

    12  - Funções.

--- index.js
+++ index.js
@@ -3,28 +3,43 @@ const prompt = require('prompt-sync')();
 var salarios = [];
 var soma = 0;
 var media;
-var qtn_menores = 0;
+var qtd_menores;
+
 
 for (var i = 1; i <= 5; i++) {
   console.log("Pessoa", i);
   var nome = prompt("Digite seu nome:");
   var salario_bruto = parseFloat(prompt(" Digite seu salário bruto:"));
   var dependentes = parseInt(prompt("Digite o número de dependentes:"));
-  var renda_percapta = salario_bruto / (dependentes + 1);
-
+  
   for (var j = 1; j <= dependentes; j++) {
     var ganho = parseFloat(prompt(" Digite seu ganho mensal:"));
     salario_bruto = salario_bruto + ganho;
   }
 
+  var renda_percapta = salario_bruto / (dependentes + 1);
 
+  var ir = calcular_ir(salario_bruto, renda_percapta)
+  var salario_liquido = salario_bruto - ir;
+  console.log("Salário líquido: R$", salario_liquido, "\n");
+  salarios.push(salario_liquido);
+  soma = soma + salario_liquido;
 
+}
+media = soma / 5
+
+qtd_menores = calcular_qtd_menores(salarios, media);
+
+console.log("A média dos salários líquidos é:", media);
+console.log("A quantidade de pessoas com salários menores que a média é:", qtd_menores);
+
+function calcular_ir(salario_bruto, renda_percapita) {
 
   if (renda_percapta >= 500) {
 
     if (salario_bruto > 0 && salario_bruto <= 1903.98) {
       var ir = salario_bruto * 0.05;
-    } else if (salario_bruto <= 28026.65) {
+    } else if (salario_bruto <= 2826.65) {
       var ir = salario_bruto * 0.075;
     } else {
       var ir = salario_bruto * 0.15;
@@ -33,19 +48,15 @@ for (var i = 1; i <= 5; i++) {
   } else {
     var ir = 0;
   }
-  var salario_liquido = salario_bruto - ir;
-  console.log("Salário líquido: R$", salario_liquido);
-  salarios.push(salario_liquido);
-  soma = soma + salario_liquido;
-
+  return ir;
 }
-media = soma / 5
 
-for (var i = 0; i <= 4; i++) {
-  if (salarios[i]) {
-    qtd_menores = qtd_menores + 1
+function calcular_qtd_menores(salarios, media) {
+  var qtd_menores = 0;
+  for (var i = 0; i <= 4; i++) {
+    if (salarios[i] < media) {
+      qtd_menores = qtd_menores + 1
+    }
   }
+  return qtd_menores;
 }
-
-console.log("A média dos salários líquidos é:", media);
-console.log("A quantidade de pessoas com salários menores que a média é:", qtd_menores);
